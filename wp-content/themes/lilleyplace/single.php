b27102c73adb5fd22f04f719c93521ad36f540ee
Trent Newton <trent99878@gmail.com>
2015-07-27 02:13:57 +1000

    forgot to add Facebook js

--- wp-content/themes/lilleyplace/single.php
+++ wp-content/themes/lilleyplace/single.php
@@ -8,6 +8,14 @@
  */
 
 get_header(); ?>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) return;
+	js = d.createElement(s); js.id = id;
+	js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.3";
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 <?php get_template_part( 'parts/banner' ); ?>
 <section class="page-content">
 	<div class="row">
