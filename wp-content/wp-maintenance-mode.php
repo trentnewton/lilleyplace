a15d1635fdf6463a10d2db0444c4c1ffa938bb23
Trent Newton <trent99878@gmail.com>
2015-07-27 23:34:23 +1000

    updated maintenance page; updated ignore list

--- wp-content/wp-maintenance-mode.php
+++ wp-content/wp-maintenance-mode.php
@@ -5,10 +5,16 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<meta name="application-name" content="Lilley Place">
 		<title>Coming Soon! | Lilley Place</title>
-<link rel='stylesheet' id='app-css'  href='https://rawgit.com/trentnewton/lilleyplace/master/wp-content/themes/lilleyplace/assets/css/app.css' type='text/css' media='all' />
-<script type='text/javascript' src='//cdnjs.cloudflare.com/ajax/libs/fastclick/1.0.6/fastclick.min.js'></script>
-		<script src="//use.typekit.net/glx7jkt.js"></script>
-    	<script>try{Typekit.load();}catch(e){}</script>
+		<meta name="application-name" content="<?php bloginfo( 'name' ); ?>">
+	    <meta name="msapplication-TileImage" content="https://rawgit.com/trentnewton/lilleyplace/master/wp-content/themes/lilleyplace/assets/img/icons/apple-touch-icon-114x114.png">
+	    <meta name="msapplication-TileColor" content="#ffffff">
+	    <link rel="icon" href="https://rawgit.com/trentnewton/lilleyplace/master/wp-content/themes/lilleyplace/assets/img/icons/favicon.ico" type="image/x-icon">
+	    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="https://rawgit.com/trentnewton/lilleyplace/master/wp-content/themes/lilleyplace/assets/img/icons/apple-touch-icon-144x144.png">
+	    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="https://rawgit.com/trentnewton/lilleyplace/master/wp-content/themes/lilleyplace/assets/img/icons/apple-touch-icon-114x114.png">
+	    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="https://rawgit.com/trentnewton/lilleyplace/master/wp-content/themes/lilleyplace/assets/img/icons/apple-touch-icon-72x72.png">
+	    <link rel="apple-touch-icon-precomposed" href="https://rawgit.com/trentnewton/lilleyplace/master/wp-content/themes/lilleyplace/assets/img/icons/apple-touch-icon-precomposed.png">
+		<link rel='stylesheet' id='app-css'  href='https://rawgit.com/trentnewton/lilleyplace/master/wp-content/themes/lilleyplace/assets/css/app.css' type='text/css' media='all' />
+		<script type='text/javascript' src='//cdnjs.cloudflare.com/ajax/libs/fastclick/1.0.6/fastclick.min.js'></script>
 	</head>
 	<body class="coming-soon">
 		<header id="main-nav">
@@ -17,26 +23,23 @@
 					<img src="https://rawgit.com/trentnewton/lilleyplace/master/wp-content/themes/lilleyplace/assets/img/images/Lilleyplace_Logo.png" width="313" height="121" alt="Lilley Place" />
 				</div>
 			</div>
-      <header id="small-banner">
-        <div class="row">
-          <div class="small-12 columns">
-              <div class="small-banner-container">
-                <h1 class="entry-title">Coming Soon&hellip;</h1>
-              </div>
-          </div>
-        </div>
-        <div class="shadow top"></div>
-        <div class="shadow bottom"></div>
-      </header>
-      <section class="page-content">
-        <div class="shadow top"></div>
-      </section>
+			<section id="small-banner">
+				<div class="row">
+				  <div class="small-12 columns">
+				      <div class="small-banner-container">
+				        <h1 class="entry-title">Coming Soon&hellip;</h1>
+				      </div>
+				  </div>
+				</div>
+				<div class="shadow top"></div>
+				<div class="shadow bottom"></div>
+			</section>
+	      	<section class="page-content">
+	        	<div class="shadow top"></div>
+	      	</section>
 		</header>
-		<script type="text/javascript">
-	        var templateUrl = '/wp-content/themes/lilleyplace';
-	    </script>
-		  <script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js'></script>
-      <script type='text/javascript' src='//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js'></script>
-      <script type='text/javascript' src='https://rawgit.com/trentnewton/lilleyplace/master/wp-content/themes/lilleyplace/themes/lilleyplace/assets/js/min/app-min.js'></script>
-			</body>
+		<script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js'></script>
+      	<script type='text/javascript' src='//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js'></script>
+      	<script type='text/javascript' src='https://rawgit.com/trentnewton/lilleyplace/master/wp-content/themes/lilleyplace/themes/lilleyplace/assets/js/min/app-min.js'></script>
+	</body>
 </html>
