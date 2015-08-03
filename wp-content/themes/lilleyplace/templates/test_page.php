5b4fa5cb44b3af383ac39e8747161210061be3cb
Trent Newton <trent99878@gmail.com>
2015-08-03 18:42:16 +1000

    another test

--- wp-content/themes/lilleyplace/templates/test_page.php
+++ wp-content/themes/lilleyplace/templates/test_page.php
@@ -1,27 +1,34 @@
 <?php
 /*
-Template Name: Test Page
+Template Name: Test
 */
 
 get_header(); ?>
 	<?php get_template_part( 'parts/banner' ); ?>
 </header>
 <?php while ( have_posts() ) : the_post(); ?>
-<div <?php post_class('page-content') ?> id="post-<?php the_ID(); ?>" style="height:600px;background:pink;">
-	<div class="footer-display">
-				<div class="row">
-					<div class="footer-content">
-						<div class="medium-5 large-6 columns">
-							<?php dynamic_sidebar("First Footer Column"); ?>
-						</div>
-						<div class="medium-5 large-4 columns">
-							<?php dynamic_sidebar("Second Footer Column"); ?>
-						</div>
-						<div class="medium-2 columns">
-							<?php dynamic_sidebar("Third Footer Column"); ?>
-						</div>
-					</div>
-				</div>
+<div <?php post_class('page-content') ?> id="post-<?php the_ID(); ?>">
+	<?php if ( is_page() && $post->post_parent > 0 ) { ?>
+   	<div class="row">
+	   	<div class="column">
+	   		<nav class="breadcrumbs-container">
+				<?php if (function_exists('site_breadcrumbs')) site_breadcrumbs(); ?>
+			</nav>
+	   	</div>
+   	</div>
+	<?php } ?>
+	<?php if ( has_post_thumbnail() ) : ?>
+		<?php the_post_thumbnail( '', array('class' => 'th mar-b-30') ); ?>
+	<?php endif; ?>
+	<?php do_action( 'lilleyplace_before_content' ); ?>
+	<article class="entry-content row">
+		<?php do_action( 'lilleyplace_page_before_entry_content' ); ?>
+		<div class="column">
+			<?php the_content(); ?>
+		</div>
+	</article>
+	<?php do_action( 'lilleyplace_after_content' ); ?>
+	<div class="shadow white-top"></div>
 </div>
 <?php endwhile;?>
 <?php get_footer(); ?>
