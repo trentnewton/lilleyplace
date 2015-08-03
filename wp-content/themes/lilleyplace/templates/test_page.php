c83130b66b0cd411a78b11ae26afe83885f31cbd
Trent Newton <trent99878@gmail.com>
2015-08-03 18:29:41 +1000

    still testing

--- wp-content/themes/lilleyplace/templates/test_page.php
+++ wp-content/themes/lilleyplace/templates/test_page.php
@@ -7,7 +7,7 @@ get_header(); ?>
 	<?php get_template_part( 'parts/banner' ); ?>
 </header>
 <?php while ( have_posts() ) : the_post(); ?>
-<main <?php post_class('page-content') ?> id="post-<?php the_ID(); ?>">
+<main <?php post_class('page-content') ?> id="post-<?php the_ID(); ?>" style="height:600px;">
 	<article class="entry-content row">
 		<div class="column">
 			<?php the_content(); ?>
