023a67ca20f5fec0e524e4b37ef2109599db8266
Trent Newton <trent99878@gmail.com>
2015-08-06 00:05:26 +1000

    added clear button to search inputs

--- wp-content/themes/lilleyplace/searchform.php
+++ wp-content/themes/lilleyplace/searchform.php
@@ -13,11 +13,11 @@ do_action( 'lilleyplace_before_searchform' ); ?>
 		<div class="column">
 			<div class="row collapse postfix-radius">
 				<?php do_action( 'lilleyplace_searchform_top' ); ?>
-				<div class="small-8 medium-10 columns">
-					<input name="s" id="s"  type="text" placeholder="<?php _e( 'Enter your search terms here', 'lilleyplace' ); ?>" value="<?php echo get_search_query(); ?>">
+				<div class="columns small-8 medium-10">
+					<input name="s" id="s" type="search" placeholder="<?php _e( 'Enter your search terms here', 'lilleyplace' ); ?>" value="<?php echo get_search_query(); ?>">
 				</div>
 				<?php do_action( 'lilleyplace_searchform_before_search_button' ); ?>
-				<div class="small-4 medium-2 columns">
+				<div class="columns small-4 medium-2">
 					<input id="searchsubmit" value="<?php _e( 'Go', 'lilleyplace' ); ?>" class="button postfix" type="submit">
 				</div>
 				<?php do_action( 'lilleyplace_searchform_after_search_button' ); ?>
