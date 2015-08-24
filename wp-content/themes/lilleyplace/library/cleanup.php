3741bade861c4aab708d0702d8e468e0eb5b7a6a
Trent Newton <trent99878@gmail.com>
2015-08-24 14:43:20 +1000

    remove yoast seo plugin update nag

--- wp-content/themes/lilleyplace/library/cleanup.php
+++ wp-content/themes/lilleyplace/library/cleanup.php
@@ -252,4 +252,50 @@ function remove_hentry( $classes ) {
 }
 add_filter( 'post_class', 'remove_hentry' );
 
+// Remove Yoast SEO nag
+
+class ahRemoveYoastNag_Remove_Yoast_SEO_Nag {
+	private $yoastPluginFile;
+	public function __construct() {
+		$this->yoastPluginFile = "wordpress-seo/wp-seo.php";
+		register_activation_hook( $this->yoastPluginFile, array( $this, 'ryn_remove_yoast_nag_on_activation' ) );
+		add_action( 'upgrader_process_complete', array( $this, 'ryn_remove_yoast_nag_after_update' ), 10, 2 );
+	}
+	/*	
+	 * Check if Yoast has been activated and remove nag
+	 */
+	function ryn_remove_yoast_nag_on_activation() {
+		// If this constant is defined we know Yoast SEO is activated
+		if ( defined( 'WPSEO_FILE' ) ) {
+			ahRemoveYoastNag_Remove_Yoast_SEO_Nag::ryn_remove_yoast_nag();
+		}
+	}
+	/*	
+	 * Remove Yoast nag after plugin has been updated
+	 */
+	function ryn_remove_yoast_nag_after_update( $upgrader_object, $options ) {
+		// Only remove the nag if Yoast SEO has been updated
+		if ( array_search( $this->yoastPluginFile, $options['plugins'] ) !==  NULL ) {
+			if ( current_user_can( 'manage_options' ) ) {
+				ahRemoveYoastNag_Remove_Yoast_SEO_Nag::ryn_remove_yoast_nag();
+			}
+		}
+	}
+	/*	
+	 * Remove nag for all Admin users
+	 */
+	private function ryn_remove_yoast_nag() {
+		$pluginData = get_plugin_data( WPSEO_FILE, false );
+		$pluginVersion = $pluginData['Version'];
+		$adminUsers = get_users( array( 'role' => 'administrator' ) );
+		// Since the nag displays for all admin users, let's clear it for all admin users
+		foreach ( $adminUsers as $user ) {
+			if ( get_user_meta( $user->ID, 'wpseo_seen_about_version', true ) !== $pluginVersion ) {
+				update_user_meta( $user->ID, 'wpseo_seen_about_version', $pluginVersion );
+			}
+		}
+	}
+}
+new ahRemoveYoastNag_Remove_Yoast_SEO_Nag();
+
 ?>
